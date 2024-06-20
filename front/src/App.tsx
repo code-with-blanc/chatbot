acd7688af8374bd04beef278a15421efe761da20
blanc <pedro6893@gmail.com>
2024-06-20 15:51:10 -0300

    start adding chat

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "./components/theme/theme-provider";
+import ChatPage from "./pages/chat/Chat";
 import Login from "./pages/login/Login"
 
 import '@/styles/globals.css';
@@ -7,7 +8,7 @@ import '@/styles/globals.css';
 function App() {
   return (
     <ThemeProvider defaultTheme="dark-blue" storageKey="ui-theme">
-      <Login />
+      <ChatPage />
     </ThemeProvider>
   )
 }
